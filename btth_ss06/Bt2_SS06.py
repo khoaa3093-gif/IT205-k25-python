2febebe8763dd1661720ea1f59358868632923b0
khoaa3093-gif <khoaa3093@gmail.com>
2026-05-28 20:46:10 +0700

    Delete btth_ss06 directory

--- btth_ss06/Bt2_SS06.py
+++
@@ -1,9 +0,0 @@
-for i in range(3):
-        password = input("Nhập mật khẩu: ")
-        if password == "123456":
-            print("Đăng nhập thành công!")
-            break
-        else:
-            print("Mật khẩu sai, vui lòng nhập lại!")
-else:             
-        print("Bạn đã đăng nhập quá 3 lần, Tài khoản đã bị khóa!")
